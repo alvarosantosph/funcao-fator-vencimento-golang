7204dec5c7d1265d9e696f6554fe7288c7fcbe85
Alvaro Santos <siruz_slipknot@hotmail.com>
2019-11-17 19:32:45 -0300

    Função para Calcular Fator de Vencimento

---
+++ funcao-fator-vencimento.go
@@ -0,0 +1,18 @@
+// Funcao para Gerar Fator de Vencimento de Acordo com a Data de Vencimento
+// Utilizar a data no formato DD/MM/AAAA
+func gerarFatorVencimento(stringData string) string {
+	runes := []rune(stringData)
+	dataParte1 := string(runes[0:2])
+	dataParte2 := string(runes[3:5])
+	dataParte3 := string(runes[6:10])
+	int1, _ := strconv.Atoi(dataParte1)
+	int2, _ := strconv.Atoi(dataParte2)
+	int3, _ := strconv.Atoi(dataParte3)
+	st := time.Date(1997, time.Month(10), 7, 00, 00, 00, 0, time.UTC)
+	en := time.Date(int3, time.Month(int2), int1, 00, 00, 00, 0, time.UTC)
+	diff := (en.Sub(st))
+	calculoMilisegundo := diff.Milliseconds() + 3600000
+	fatorVencimento := calculoMilisegundo / 86400000
+	retorno := strconv.FormatInt(fatorVencimento, 10)
+	return retorno
+}
